95d2842d5bc4f8ccad18d28a0d30656c7406ff0f
EkaTur <savosya23@gmail.com>
2024-02-21 22:22:41 +0100

    Added toml

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import React from 'react';
+import gsap from 'gsap';
 import './App.css';
 
 function App() {
+
+  const [newAdvice, setNewAdvice] = useState('');
+
+  useLayoutEffect(() => {
+    gsap.fromTo('.box-3', {y: -50, opacity: 0}, {y: 40, opacity: 1, duration: 3})
+
+  }, [])
+
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  const handleClick = () => {
+    gsap.fromTo('.box-1', { rotation: 0 }, { rotation: 360 });
+    gsap.fromTo('.box-2', { scale: 0 }, { scale: 2 });
+  }
+
+  const getAdvice = async () => {
+    const response = await fetch(`https://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    console.log(data.activity);
+    setNewAdvice(data.activity);
+} 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='Container'>
+        <h1 className='box-3'>Are you bored and you don't know what to do?</h1>
+      </div>
+      <div className='Container'>
+        <p>Get advice and be busy!</p>
+      </div>
+      <div className='Container'>
+        <button className='box-1' onClick={() => { getAdvice(); handleClick(); }}>CLICK HERE!</button>
+      </div>
+      <div className='Container'>
+        <h2 className='Advice box-2'>{newAdvice}</h2>
+      </div>
     </div>
   );
 }
